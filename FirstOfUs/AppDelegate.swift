9f4b416fb933b166624c981e8434b818ea611171
Stas Kirichok <stas.kirichok@windmill.ch>
2018-11-01 22:29:36 +0200

    Initial Commit

---
+++ FirstOfUs/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  FirstOfUs
+//
+//  Created by Stas Kirichok on 01-11-2018.
+//  Copyright © 2018 Stas Kirichok. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
